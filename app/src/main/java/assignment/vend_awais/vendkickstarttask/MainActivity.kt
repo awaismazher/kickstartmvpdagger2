4ed34eae947b680e0b05cc0a179e6d301afbfa83
syed.awais.mazhar <syedawaismazhar@gmail.com>
2018-01-08 15:19:09 +0500

    - cloned code setup

---
+++ app/src/main/java/assignment/vend_awais/vendkickstarttask/MainActivity.kt
@@ -0,0 +1,14 @@
+package assignment.vend_awais.vendkickstarttask
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import assignment.vend_awais.vendkickstarttask.api.KickStartApi
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
---
+++ app/src/main/java/assignment/vend_awais/vendkickstarttask/util/Constants.java
@@ -0,0 +1,12 @@
+package assignment.vend_awais.vendkickstarttask.util;
+
+/**
+ * Created by syed.awais.mazhar on 1/8/2018.
+ */
+
+public class Constants {
+    private Constants(){
+
+    }
+    public static final String API_KEY = "92a217f2eab562211221d5568f133e01";
+}

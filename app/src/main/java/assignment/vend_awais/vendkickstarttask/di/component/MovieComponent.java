9afd2344af7cd77dd8ef54aaecef5e04651011a7
syed.awais.mazhar <syedawaismazhar@gmail.com>
2018-01-11 14:58:19 +0500

    - refacored movie compnent

--- app/src/main/java/assignment/vend_awais/vendkickstarttask/di/component/MovieComponent.java
+++ app/src/main/java/assignment/vend_awais/vendkickstarttask/di/component/MovieComponent.java
@@ -1,21 +1,14 @@
 package assignment.vend_awais.vendkickstarttask.di.component;
 
-import android.support.v7.widget.RecyclerView;
-
+import assignment.vend_awais.vendkickstarttask.di.module.NetModule;
 import assignment.vend_awais.vendkickstarttask.di.scope.ActivityScope;
 import assignment.vend_awais.vendkickstarttask.di.module.MovieModule;
-import assignment.vend_awais.vendkickstarttask.movies.adapter.MovieAdapter;
 import assignment.vend_awais.vendkickstarttask.movies.view.activity.MovieActivity;
-import assignment.vend_awais.vendkickstarttask.movies.view.presenter.Presenter;
 import dagger.Component;
 
 @ActivityScope
-@Component(dependencies = AppComponent.class, modules = MovieModule.class)
+@Component(dependencies = AppComponent.class, modules = {MovieModule.class, NetModule.class})
 public interface MovieComponent {
   void inject(MovieActivity movieActivity);
 
-  Presenter
-  getMainPresenter();
-  RecyclerView.LayoutManager getLinearLayoutManager();
-  MovieAdapter getMovieAdapter();
 }

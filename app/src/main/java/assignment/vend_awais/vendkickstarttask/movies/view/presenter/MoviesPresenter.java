3bf1ccb5a50d2c40bec4d8ec282316cec8606a4c
syed.awais.mazhar <syedawaismazhar@gmail.com>
2018-01-11 14:59:44 +0500

    - modified presenter - implemented Observer for retrofit call

--- app/src/main/java/assignment/vend_awais/vendkickstarttask/movies/view/presenter/MoviesPresenter.java
+++ app/src/main/java/assignment/vend_awais/vendkickstarttask/movies/view/presenter/MoviesPresenter.java
@@ -2,24 +2,53 @@ package assignment.vend_awais.vendkickstarttask.movies.view.presenter;
 
 import java.util.List;
 
+import javax.inject.Inject;
+
+import assignment.vend_awais.vendkickstarttask.api.IWebServiceKickStart;
+import assignment.vend_awais.vendkickstarttask.api.MoviesListResponseModel;
 import assignment.vend_awais.vendkickstarttask.movies.model.Callback;
 import assignment.vend_awais.vendkickstarttask.movies.model.Movie;
 import assignment.vend_awais.vendkickstarttask.movies.model.Movies;
+import rx.Observable;
+import rx.Observer;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
-public class MoviesPresenter implements Presenter, Callback {
+import static assignment.vend_awais.vendkickstarttask.util.Constants.API_KEY;
 
+public class MoviesPresenter implements Presenter, Callback, Observer<MoviesListResponseModel> {
   private PresenterView presenterView;
-  private Movies movies;
+  //private Movies movies;
+  @Inject
+  IWebServiceKickStart iWebServiceKickStart;
 
-  public MoviesPresenter(PresenterView presenterView, Movies movies) {
-    this.presenterView = presenterView;
-    this.movies = movies;
+  @Inject
+  public MoviesPresenter() {
+
   }
 
   @Override
   public void onResume() {
     presenterView.showProgress();
-    movies.getCategories(this);
+    //movies.getCategories(this);
+    getCategoriesViaObservable();
+  }
+
+  @Override
+  public void setView(PresenterView presenterView) {
+   this.presenterView = presenterView;
+  }
+
+  private void getCategoriesViaObservable() {
+    Observable<MoviesListResponseModel> moviesObservable = iWebServiceKickStart.getMovieList(API_KEY);
+    subscribe(moviesObservable,this);
+  }
+
+  private void subscribe(Observable<MoviesListResponseModel> observable, Observer<MoviesListResponseModel> observer) {
+    observable.subscribeOn(Schedulers.newThread())
+            .toSingle()
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(observer);
   }
 
   @Override
@@ -31,6 +60,22 @@ public class MoviesPresenter implements Presenter, Callback {
   public void onLoadCategories(List<Movie> movies) {
     presenterView.showCategories(movies);
     presenterView.hideProgress();
+  }
+
+  @Override
+  public void onCompleted() {
+    presenterView.hideProgress();
+  }
+
+  @Override
+  public void onError(Throwable e) {
+    presenterView.showMessage(e.getMessage());
+    presenterView.hideProgress();
+  }
+
+  @Override
+  public void onNext(MoviesListResponseModel moviesListResponseModel) {
+    presenterView.showCategories(moviesListResponseModel.getMovies());
   }
 
   public interface PresenterView {

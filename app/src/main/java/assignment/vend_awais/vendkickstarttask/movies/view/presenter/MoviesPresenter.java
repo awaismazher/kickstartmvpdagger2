f292a54c8e8f453621c40a3c450e6cee50ee0f34
syed.awais.mazhar <syedawaismazhar@gmail.com>
2018-01-11 16:14:08 +0500

    - removed un used code - added dependencies for mockito and test cases

--- app/src/main/java/assignment/vend_awais/vendkickstarttask/movies/view/presenter/MoviesPresenter.java
+++ app/src/main/java/assignment/vend_awais/vendkickstarttask/movies/view/presenter/MoviesPresenter.java
@@ -6,9 +6,7 @@ import javax.inject.Inject;
 
 import assignment.vend_awais.vendkickstarttask.api.IWebServiceKickStart;
 import assignment.vend_awais.vendkickstarttask.api.MoviesListResponseModel;
-import assignment.vend_awais.vendkickstarttask.movies.model.Callback;
 import assignment.vend_awais.vendkickstarttask.movies.model.Movie;
-import assignment.vend_awais.vendkickstarttask.movies.model.Movies;
 import rx.Observable;
 import rx.Observer;
 import rx.android.schedulers.AndroidSchedulers;
@@ -16,76 +14,69 @@ import rx.schedulers.Schedulers;
 
 import static assignment.vend_awais.vendkickstarttask.util.Constants.API_KEY;
 
-public class MoviesPresenter implements Presenter, Callback, Observer<MoviesListResponseModel> {
-  private PresenterView presenterView;
-  //private Movies movies;
-  @Inject
-  IWebServiceKickStart iWebServiceKickStart;
+public class MoviesPresenter implements Presenter, Observer<MoviesListResponseModel> {
+    private PresenterView presenterView;
+    @Inject
+    IWebServiceKickStart iWebServiceKickStart;
 
-  @Inject
-  public MoviesPresenter() {
+    @Inject
+    public MoviesPresenter() {
 
-  }
+    }
 
-  @Override
-  public void onResume() {
-    presenterView.showProgress();
-    //movies.getCategories(this);
-    getCategoriesViaObservable();
-  }
+    @Override
+    public void onResume() {
+        presenterView.showProgress();
+        //movies.getCategories(this);
+        getCategoriesViaObservable();
+    }
 
-  @Override
-  public void setView(PresenterView presenterView) {
-   this.presenterView = presenterView;
-  }
+    @Override
+    public void setView(PresenterView presenterView) {
+        this.presenterView = presenterView;
+    }
 
-  private void getCategoriesViaObservable() {
-    Observable<MoviesListResponseModel> moviesObservable = iWebServiceKickStart.getMovieList(API_KEY);
-    subscribe(moviesObservable,this);
-  }
+    private void getCategoriesViaObservable() {
+        Observable<MoviesListResponseModel> moviesObservable = iWebServiceKickStart.getMovieList(API_KEY);
+        subscribe(moviesObservable, this);
+    }
 
-  private void subscribe(Observable<MoviesListResponseModel> observable, Observer<MoviesListResponseModel> observer) {
-    observable.subscribeOn(Schedulers.newThread())
-            .toSingle()
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe(observer);
-  }
+    private void subscribe(Observable<MoviesListResponseModel> observable, Observer<MoviesListResponseModel> observer) {
+        observable.subscribeOn(Schedulers.newThread())
+                .toSingle()
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(observer);
+    }
 
-  @Override
-  public void onItemSelected(Movie movie, int position) {
-    presenterView.showMessage(String.format(movie.getTitle() + " ->" + " Position %d clicked", position));
-  }
+    @Override
+    public void onItemSelected(Movie movie, int position) {
+        presenterView.showMessage(String.format(movie.getTitle() + " ->" + " Position %d clicked", position));
+    }
 
-  @Override
-  public void onLoadCategories(List<Movie> movies) {
-    presenterView.showCategories(movies);
-    presenterView.hideProgress();
-  }
+    @Override
+    public void onCompleted() {
+        presenterView.hideProgress();
+    }
 
-  @Override
-  public void onCompleted() {
-    presenterView.hideProgress();
-  }
+    @Override
+    public void onError(Throwable e) {
+        presenterView.showMessage(e.getMessage());
+        presenterView.hideProgress();
+    }
 
-  @Override
-  public void onError(Throwable e) {
-    presenterView.showMessage(e.getMessage());
-    presenterView.hideProgress();
-  }
+    @Override
+    public void onNext(MoviesListResponseModel moviesListResponseModel) {
+        presenterView.showCategories(moviesListResponseModel.getMovies());
+    }
 
-  @Override
-  public void onNext(MoviesListResponseModel moviesListResponseModel) {
-    presenterView.showCategories(moviesListResponseModel.getMovies());
-  }
+    public interface PresenterView {
 
-  public interface PresenterView {
+        void showProgress();
 
-    void showProgress();
+        void hideProgress();
 
-    void hideProgress();
+        void showCategories(List<Movie> categories);
 
-    void showCategories(List<Movie> categories);
-
-    void showMessage(String message);
-  }
+        void showMessage(String message);
+    }
 }
